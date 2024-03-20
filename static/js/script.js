55ff48fe1ce98b4ad25c780c1ae1f9546f5a62e4
pembattech <pembat.tech@gmail.com>
2024-03-20 15:41:53 +0545

    Add register.html and active nav fix.

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,47 @@
 $(document).ready(function () {
-            function showPopup() {
-                $('#popup').fadeIn();
-            }
-
-            function hidePopup() {
-                $('#popup').fadeOut();
-            }
-
-            if ($('.home-tab').hasClass('active')) {
-                showPopup();
-            }
-
-            $('.nav-list a').click(function () {
-                $('.nav-list a').removeClass('active');
-                $(this).addClass('active');
+    const navLinks = document.querySelectorAll('.nav-link');
+    const currentPath = window.location.pathname;
+    navLinks.forEach(link => {
+        if (link.href.endsWith(currentPath)) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
 
 
-                if ($(this).hasClass('home-tab')) {
-                    showPopup();
-                } else {
-                    hidePopup();
-                }
-            });
+    function showPopup() {
+        $('#popup').fadeIn();
+    }
+
+    function hidePopup() {
+        $('#popup').fadeOut();
+    }
+
+    if ($('.home-tab').hasClass('active')) {
+        showPopup();
+    }
+
+    $('.nav-list a').click(function () {
+        $('.nav-list a').removeClass('active');
+        $(this).addClass('active');
 
 
-            $('#closePopup').click(function () {
-                hidePopup();
-            });
+        if ($(this).hasClass('home-tab')) {
+            showPopup();
+        } else {
+            hidePopup();
+        }
+    });
 
-            $(document).keydown(function (e) {
-                if (e.keyCode === 27) {
-                    hidePopup();
-                }
-            });
-        });
+
+    $('#closePopup').click(function () {
+        hidePopup();
+    });
+
+    $(document).keydown(function (e) {
+        if (e.keyCode === 27) {
+            hidePopup();
+        }
+    });
+});
