7cadb60048aa040143cf02f05a0199af0d41b7c8
pembattech <pembat.tech@gmail.com>
2024-05-04 10:10:36 +0545

    Add dark mode func.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
+    // Check if there's a theme preference in local storage
+    const theme = localStorage.getItem('theme');
+
+    // Apply theme if it exists in local storage
+    if (theme) {
+        $('body').addClass(theme);
+    }
+
+    // Toggle theme on click
+    $(".theme-switch").on("click", function () {
+        $("body").toggleClass("dark-mode");
+
+        // Save theme preference to local storage
+        const currentTheme = $("body").hasClass("dark-mode") ? "dark-mode" : "";
+        localStorage.setItem('theme', currentTheme);
+    });
+
+
     const navLinks = document.querySelectorAll('.nav-link');
     const currentPath = window.location.pathname;
     navLinks.forEach(link => {
@@ -9,41 +27,39 @@ $(document).ready(function () {
         }
     });
 
+    function showPopup() {
+        $('#popup').fadeIn();
+    }
 
-    
+    function hidePopup() {
+        $('#popup').fadeOut();
+    }
 
-    // function showPopup() {
-    //     $('#popup').fadeIn();
-    // }
+    if ($('.home-tab').hasClass('active')) {
+        showPopup();
+    }
 
-    // function hidePopup() {
-    //     $('#popup').fadeOut();
-    // }
-
-    // if ($('.home-tab').hasClass('active')) {
-    //     showPopup();
-    // }
-
-    // $('.nav-list a').click(function () {
-    //     $('.nav-list a').removeClass('active');
-    //     $(this).addClass('active');
+    $('.nav-list a').click(function () {
+        $('.nav-list a').removeClass('active');
+        $(this).addClass('active');
 
 
-    //     if ($(this).hasClass('home-tab')) {
-    //         showPopup();
-    //     } else {
-    //         hidePopup();
-    //     }
-    // });
+        if ($(this).hasClass('home-tab')) {
+            showPopup();
+        } else {
+            hidePopup();
+        }
+    });
 
 
-    // $('#closePopup').click(function () {
-    //     hidePopup();
-    // });
+    $('#closePopup').click(function () {
+        hidePopup();
+    });
 
-    // $(document).keydown(function (e) {
-    //     if (e.keyCode === 27) {
-    //         hidePopup();
-    //     }
-    // });
+    $(document).keydown(function (e) {
+        if (e.keyCode === 27) {
+            hidePopup();
+        }
+    });
+
 });
