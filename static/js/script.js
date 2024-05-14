6c5a86823a546dec1aabf98dae5a0a9c7da87135
pembattech <pembat.tech@gmail.com>
2024-05-14 09:45:41 +0545

    Add custom scrollbar.

--- static/js/script.js
+++ static/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         $("body").toggleClass("dark-mode");
 
         // Save theme preference to local storage
-        const currentTheme = $("body").hasClass("dark-mode") ? "biswo" : "";
+        const currentTheme = $("body").hasClass("dark-mode") ? "dark-mode" : "";
         localStorage.setItem('theme', currentTheme);
     });
 
