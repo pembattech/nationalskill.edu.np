a81ee5b67eba1ac19e744180c446451f7a371a04
pembattech <pembat.tech@gmail.com>
2026-03-07 20:40:52 +0545

    Minor chores fix.

--- static/js/script.js
+++ static/js/script.js
@@ -1,75 +1,9 @@
-$(document).ready(function () {
-    // Check if there's a theme preference in local storage
-    const theme = localStorage.getItem('theme');
+document.getElementById('year').textContent = new Date().getFullYear();
 
+// Hamburger
+const mob = document.getElementById('mobileMenu');
 
-    $("#currentyear").text(new Date().getFullYear());
-
-    // Apply theme if it exists in local storage
-    if (theme) {
-        $('body').addClass(theme);
-    }
-
-    // Toggle theme on click
-    $(".theme-switch").on("click", function () {
-        $("body").toggleClass("dark-mode");
-        const currentTheme = $("body").hasClass("dark-mode") ? "dark-mode" : "";
-        localStorage.setItem('theme', currentTheme);
-    });
-
-    // const navLinks = document.querySelectorAll('.nav-link');
-    // const currentPath = window.location.pathname;
-    // navLinks.forEach(link => {
-    //     if (link.getAttribute('href').endsWith(currentPath)) {
-    //         link.classList.add('active');
-    //     } else {
-    //         link.classList.remove('active');
-    //     }
-    // });
-
-    function enableSidePusher() {
-        $('.container').addClass('fixed-height');
-    }
-
-    function disableSidePusher() {
-        $('.container').removeClass('fixed-height');
-    }
-
-    $("#header__icon").click(function (e) {
-        e.preventDefault();
-        $("body").toggleClass("with--sidebar");
-    });
-
-    $("#site-cache").click(function (e) {
-        $("body").removeClass("with--sidebar");
-        // disableSidePusher();
-    });
-
-    function showPopup() {
-        $('#popup').fadeIn();
-    }
-
-    function hidePopup() {
-        $('#popup').fadeOut();
-    }
-
-    const navLinks = document.querySelectorAll('.nav-link');
-    const currentPath = window.location.pathname;
-    navLinks.forEach(link => {
-        if (link.getAttribute('href') == '/' || currentPath == '/' || link.getAttribute('href').endsWith(currentPath)) {
-            showPopup();
-        }
-    });
-
-
-    $('#closePopup').click(function () {
-        hidePopup();
-    });
-
-    $(document).keydown(function (e) {
-        if (e.keyCode === 27) {
-            hidePopup();
-        }
-    });
-
-});
+document.getElementById('hamburger').onclick = () => {
+    mob.classList.toggle('-translate-y-full');
+    mob.classList.toggle('translate-y-0');
+};
