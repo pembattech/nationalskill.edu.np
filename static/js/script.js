43b53fcd7fbc75e4d1f6506645eb0eafb63a798a
pembattech <pembat.tech@gmail.com>
2026-04-18 16:53:40 +0545

    chores update.

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,19 @@ document.getElementById('hamburger').onclick = () => {
     mob.classList.toggle('-translate-y-full');
     mob.classList.toggle('translate-y-0');
 };
+
+
+fetch("links.json")
+    .then(response => response.json())
+    .then(data => {
+
+        document.querySelectorAll(".loginLink").forEach(el => {
+            el.href = data.login;
+        });
+
+        document.querySelectorAll(".registerLink").forEach(el => {
+            el.href = data.register;
+        });
+
+    })
+    .catch(error => console.error("Error loading JSON:", error));
